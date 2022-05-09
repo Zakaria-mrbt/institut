8624ab1cdf5b13b5c3a6fa256aa9d81f4239dca2
13010-30-07 <13010-30-07@exchange.ad.afpanet>
2022-05-09 16:35:52 +0200

    ajout header

---
+++ header.php
@@ -0,0 +1,40 @@
+<body>
+<header>
+   <div class="header1">
+       <div class="lb_1-2">
+         <a href="#"> <img class="essentielle" src="img/logo3.png" alt="essentielle"></a>
+         <div class="adresse">
+             
+        <button class="btn_adresse" type="button"> <div class='imgg'></div><a href="#" class="adresse_links"> 76 Boulevard Eugène Pierre, 13005 Marseille, France</a></button>
+         </div> 
+       
+       </div>
+       
+       
+       <div class="lb_3-5">
+       <a href="#" target="_blank" ><img class="provencesanté" src="img/logo_provence-sante.png" alt="Provence santé"></a>
+       <a href="#" target="_blank" ><img class="bloomea" src="img/logo-bloomea-light2.png" alt="bloomea"></a>
+       <a href="#" target="_blank" ><img class="allonature" src="img/allo-nature-logo.png" alt="allo nature"></a>
+    </div>
+       
+   </div>
+   <div class="header2">
+
+  <ul class="navlink">
+       <li class="navlinks" ><a href="#">Acceuil</a></li>
+       <li class="navlinks" ><a href="#">Epilations</a></li>
+       <li class="navlinks" ><a href="#">Main et pieds</a></li>
+       <li class="navlinks" ><a href="#">Maquillage</a></li>
+       <li class="navlinks" ><a href="#">Soin visage</a></li>
+       <li class="navlinks" ><a href="#">Soin corps</a></li>
+       <li class="navlinks" ><a href="#">Amincissement</a></li>
+       <li class="navlinks" ><a href="#">Bon cadeau</a></li>
+       <li class="navlinks" ><a href="#">Nous contacter</a></li>
+    </ul>  
+<span></span>
+
+   
+    
+   </div>
+   
+</header>
